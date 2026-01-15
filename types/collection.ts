cd8083c1f4b2626853d79224b99edef906160fa4
Yusi <yusi4781@naver.com>
2026-01-15 20:20:12 +0900

    feat(collection): 컬렉션 장소 목데이터 추가 및 타입 정의 [WAPO-83]

--- types/collection.ts
+++ types/collection.ts
@@ -1,7 +1,16 @@
+export interface Place {
+  placeName: string;
+  placeDescription: string;
+  placeAddress: string;
+  likeCount: number;
+  dislikeCount: number;
+}
+
 export interface Collection {
   collectionId: number;
   title: string;
   destination: string;
   memberCount: number;
   thumbnailImageUrl: string;
+  places: Place[];
 }
