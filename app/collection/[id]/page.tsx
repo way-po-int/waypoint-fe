20f7e4fcc80781fea2185add4c1e16faa68f10a4
Yusi <yusi4781@naver.com>
2026-01-15 21:08:58 +0900

    feat(collection): 콜렉션 상세 페이지 헤더 레이아웃 추가 [WAPO-83]

--- app/collection/[id]/page.tsx
+++ app/collection/[id]/page.tsx
@@ -1,5 +1,30 @@
+"use client";
+
+import Header from "@/components/layout/Header";
+import { collectionMockData } from "@/mocks/collectionMockData";
+import { Collection } from "@/types/collection";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+
 const CollectionPage = () => {
-  return <div>CollectionPage</div>;
+  const params = useParams();
+  const collectionId = params.id as string;
+  const [collection, setCollection] = useState<Collection | null>(
+    collectionMockData.find(
+      (collection) => collection.collectionId === parseInt(collectionId)
+    ) || null
+  );
+
+  return (
+    <div>
+      <Header
+        variant="center"
+        title={collection?.title || ""}
+        showBackButton
+        showNotificationButton
+      />
+    </div>
+  );
 };
 
 export default CollectionPage;
