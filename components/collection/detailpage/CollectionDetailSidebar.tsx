bfaf49928d90daa7dcd34c2f24b9e2184def74bf
Yusi <yusi4781@naver.com>
2026-01-18 04:31:08 +0900

    feat(sidebar): 여행 멤버 초대하기 팝업 구현 [WAPO-84]
    - 초대하기 버튼을 누르면 여행 멤버 초대하기 팝업이 뜹니다.
    - 초대 링크 복사 버튼을 누르면 하단에 sonner를 사용하여 toast 메시지를 2초간 띄웁니다.

--- components/collection/detailpage/CollectionDetailSidebar.tsx
+++ components/collection/detailpage/CollectionDetailSidebar.tsx
@@ -3,6 +3,14 @@
 import Divider from "@/components/common/Divider";
 import { Button } from "@/components/ui/button";
 import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
   DrawerClose,
   DrawerContent,
   DrawerHeader,
@@ -15,8 +23,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { EllipsisIcon, X } from "lucide-react";
+import { Copy, EllipsisIcon, UserPlusIcon, X } from "lucide-react";
 import { useState } from "react";
+import { toast } from "sonner";
 
 interface CollectionDetailSidebarProps {
   members: string[];
@@ -24,6 +33,7 @@ interface CollectionDetailSidebarProps {
 
 const CollectionDetailSidebar = ({ members }: CollectionDetailSidebarProps) => {
   const [isManageBtnClicked, setIsManageBtnClicked] = useState<boolean>(false);
+  const inviteLink = "http://aaa.bb/Dkdsg7xcbjfsdD";
 
   const handleRemoveMember = (member: string) => {
     console.log(`${member} 내보내기 클릭됨`);
@@ -33,6 +43,19 @@ const CollectionDetailSidebar = ({ members }: CollectionDetailSidebarProps) => {
   const handleMakeOwner = (member: string) => {
     console.log(`${member} 컬렉션 소유자로 지정 클릭됨`);
     //TODO: 컬렉션 소유자로 지정 기능 구현
+  };
+
+  const handleCopyInviteLink = async () => {
+    try {
+      await navigator.clipboard.writeText(inviteLink);
+      toast.success("초대 링크가 복사되었습니다", {
+        icon: <Copy className="size-4" />,
+        duration: 2000,
+      });
+    } catch (err) {
+      console.error("클립보드 복사 실패:", err);
+      toast.error("복사에 실패했습니다");
+    }
   };
 
   return (
@@ -82,7 +105,9 @@ const CollectionDetailSidebar = ({ members }: CollectionDetailSidebarProps) => {
                         내보내기
                       </DropdownMenuItem>
                       <DropdownMenuSeparator />
-                      <DropdownMenuItem>컬렉션 소유자로 지정</DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => handleMakeOwner(member)}>
+                        컬렉션 소유자로 지정
+                      </DropdownMenuItem>
                     </DropdownMenuContent>
                   </DropdownMenu>
                 )}
@@ -91,9 +116,30 @@ const CollectionDetailSidebar = ({ members }: CollectionDetailSidebarProps) => {
           </div>
           <div className="flex flex-col gap-3.5">
             <Divider />
-            <Button className="w-full bg-slate-300 text-gray-900">
-              초대하기
-            </Button>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button className="w-full bg-slate-300 text-gray-900">
+                  초대하기
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader className="text-left">
+                  <DialogTitle className="text-lg font-bold">
+                    여행 멤버 초대하기
+                  </DialogTitle>
+                </DialogHeader>
+                <DialogDescription className="text-xs text-gray-500">
+                  우리 여행에 함께할 멤버를 초대해요!
+                </DialogDescription>
+                <div className="flex flex-col items-center justify-center gap-7">
+                  <UserPlusIcon className="size-21" strokeWidth={0.5} />
+                  <p className="text-sm font-normal">{inviteLink}</p>
+                  <Button className="w-full" onClick={handleCopyInviteLink}>
+                    초대 링크 복사
+                  </Button>
+                </div>
+              </DialogContent>
+            </Dialog>
           </div>
         </div>
         <div className="flex flex-col gap-3">
