fcdf054fa8eb7525a36fdfee5209fd66e4e5dcf7
Yusi <yusi4781@naver.com>
2026-01-17 19:18:28 +0900

    feat(sidebar): 콜렉션 디테일 페이지 사이드바 멤버 관리 버튼 기능 구현 [WAPO-84]
    - 관리 버튼 클릭 시 각 멤버 오른쪽에 ellipsis 아이콘이 뜹니다.
    - 이 버튼 클릭 시 dropdown menu가 나옵니다.
    - dropdown menu 클릭 기능은 미구현 상태이며, TODO로 작성해놨습니다.

--- components/collection/detailpage/CollectionDetailSidebar.tsx
+++ components/collection/detailpage/CollectionDetailSidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Divider from "@/components/common/Divider";
 import { Button } from "@/components/ui/button";
 import {
@@ -6,12 +8,33 @@ import {
   DrawerHeader,
   DrawerTitle,
 } from "@/components/ui/drawer";
-import { X } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { EllipsisIcon, X } from "lucide-react";
+import { useState } from "react";
 
 interface CollectionDetailSidebarProps {
   members: string[];
 }
+
 const CollectionDetailSidebar = ({ members }: CollectionDetailSidebarProps) => {
+  const [isManageBtnClicked, setIsManageBtnClicked] = useState<boolean>(false);
+
+  const handleRemoveMember = (member: string) => {
+    console.log(`${member} 내보내기 클릭됨`);
+    //TODO: 내보내기 기능 구현
+  };
+
+  const handleMakeOwner = (member: string) => {
+    console.log(`${member} 컬렉션 소유자로 지정 클릭됨`);
+    //TODO: 컬렉션 소유자로 지정 기능 구현
+  };
+
   return (
     <DrawerContent>
       <DrawerHeader className="flex items-end w-full p-3">
@@ -24,7 +47,14 @@ const CollectionDetailSidebar = ({ members }: CollectionDetailSidebarProps) => {
       <div className="flex flex-col gap-2 mt-2 px-2">
         <DrawerTitle className="flex items-center justify-between gap-2">
           <p className="text-base font-bold">여행 멤버</p>
-          <Button variant="outline">관리</Button>
+          {!isManageBtnClicked && (
+            <Button
+              variant="outline"
+              onClick={() => setIsManageBtnClicked(true)}
+            >
+              관리
+            </Button>
+          )}
         </DrawerTitle>
         <div className="flex flex-col gap-3 rounded-1.5 border border-[#e4e4e7] p-4">
           <div className="flex flex-col p-1">
@@ -32,9 +62,30 @@ const CollectionDetailSidebar = ({ members }: CollectionDetailSidebarProps) => {
               <p className=" text-[#71717a] text-xs">여행 멤버</p>
             </div>
             {members.map((member) => (
-              <div key={member} className="p-2 flex gap-2">
-                <div className="size-5 rounded-full bg-[#d9d9d9]" />
-                <p className="text-sm font-normal">{member}</p>
+              <div key={member} className="flex justify-between items-center">
+                <div className="p-2 flex gap-2">
+                  <div className="size-5 rounded-full bg-[#d9d9d9]" />
+                  <p className="text-sm font-normal">{member}</p>
+                </div>
+                {isManageBtnClicked && (
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="ghost" className="w-6 h-6">
+                        <EllipsisIcon className="size-6" />
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuItem
+                        className="text-sm font-semibold"
+                        onClick={() => handleRemoveMember(member)}
+                      >
+                        내보내기
+                      </DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>컬렉션 소유자로 지정</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                )}
               </div>
             ))}
           </div>
