7c55e76d59e48c346322ff24641c28c83670dd6e
Yusi <yusi4781@naver.com>
2026-01-24 05:12:55 +0900

    fix(collectionDetail): mock data 수정에 따른 콜렉션 상세 페이지 에러 수정 [WAPO-103]

--- components/collection/detailpage/PlaceCard.tsx
+++ components/collection/detailpage/PlaceCard.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Place } from "@/types/collection";
+import { CollectionPlace } from "@/types/collection";
 import {
   BanIcon,
   EllipsisIcon,
@@ -7,17 +7,22 @@ import {
   MapPinIcon,
 } from "lucide-react";
 
-const PlaceCard = ({ place }: { place: Place }) => {
+const PlaceCard = ({
+  collectionPlace,
+}: {
+  collectionPlace: CollectionPlace;
+}) => {
+  const { place, memo, like_count, dislike_count } = collectionPlace;
   return (
     <div className="flex flex-col gap-2.5 bg-slate-200 p-2.5 rounded-xl">
       <div className="flex justify-between items-center">
-        <p className="text-base font-bold">{place.placeName}</p>
+        <p className="text-base font-bold">{place.name}</p>
         <Button variant="ghost">
           <EllipsisIcon className="size-6" />
         </Button>
       </div>
       <p className="text-sm overflow-hidden text-ellipsis whitespace-nowrap">
-        {place.placeDescription}
+        {memo}
       </p>
       <div className="h-22 bg-white flex items-center justify-center">
         사진이 들어갈 공간입니다.
@@ -25,7 +30,9 @@ const PlaceCard = ({ place }: { place: Place }) => {
       <div className="flex flex-row w-full justify-between items-center gap-2">
         <div className="flex gap-[7px] items-center min-w-0 flex-1">
           <MapPinIcon className="size-4.5 flex-shrink-0" />
-          <p className="text-sm overflow-hidden text-ellipsis whitespace-nowrap min-w-0">{place.placeAddress}</p>
+          <p className="text-sm overflow-hidden text-ellipsis whitespace-nowrap min-w-0">
+            {place.address}
+          </p>
         </div>
         <div className="flex gap-2.5 flex-shrink-0">
           <Button
@@ -33,14 +40,14 @@ const PlaceCard = ({ place }: { place: Place }) => {
             className="bg-slate-100 rounded-2 px-[3px] py-[2px] gap-2.5"
           >
             <FlagTriangleRight className="size-5" />
-            <p className="text-sm">{place.likeCount}</p>
+            <p className="text-sm">{like_count}</p>
           </Button>
           <Button
             variant="ghost"
             className="bg-slate-100 rounded-2 px-[3px] py-[2px] gap-2.5"
           >
             <BanIcon className="size-5" />
-            <p className="text-sm">{place.dislikeCount}</p>
+            <p className="text-sm">{dislike_count}</p>
           </Button>
         </div>
       </div>
