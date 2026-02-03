756bc02d1fa5c9701b410dae02797553695feeff
projectmiluju <dnjsdyd0712@naver.com>
2026-02-03 15:53:13 +0900

    feat(ExpenseDivider): 새로운 지출 구분 컴포넌트 추가 및 UI 구성 [WAPO-136]

---
+++ components/projects/budget/ExpenseDivider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const ExpenseDivider = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col items-center">
+        <div className="h-[10px] w-px bg-[#94A3B8]" />
+        <div className="h-[10px] w-px bg-[#94A3B8]" />
+      </div>
+    </div>
+  );
+};
